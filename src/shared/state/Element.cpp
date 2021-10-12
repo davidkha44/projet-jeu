c933725afe5441676e836ae8996d22d4b5e2d9e5
PALMIER Florian <palmier.florian@gmail.com>
2021-10-12 13:49:00 +0200

    WIP : classes

---
+++ src/shared/state/Element.cpp
@@ -0,0 +1,4 @@
+#include "Element.h"
+
+Element::Element(int x,int y,int id): x(x),y(y),id(id){};
+
---
+++ src/shared/state/Space.cpp
@@ -0,0 +1,11 @@
+#include "Space.h"
+#include <fstream>
+#include <iostream>
+#include <sstream>
+#include <memory>
+#include <vector>
+
+using namespace std;
+using namespace state;
+
+Space::Space(SpaceTypeId id){};
