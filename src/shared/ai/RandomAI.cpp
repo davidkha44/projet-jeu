ca26edb56eba848f67ba65861bd43b6073fb70bc
HU Mazhou <mazhouhu97@gmail.com>
2021-11-17 15:55:10 +0100

    initiale AI

---
+++ src/shared/ai/RandomAI.cpp
@@ -0,0 +1,12 @@
+//#include "engine.h"
+#include "state.h"
+#include <iostream>
+#include <stdlib.h>
+
+
+using namespace state;
+using namespace std;
+
+
+//void RandomAI :: run(engine ::Engine &engine){}
+    
