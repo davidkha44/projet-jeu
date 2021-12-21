aff648cffa0ba209fcf3ba1f933ab624122b764e
David Kha <david.kha44@gmail.com>
2021-12-21 18:48:00 +0100

    server executable

---
+++ src/server/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string.h>
+#include <unistd.h>
+
+using namespace std;
+
+int main(int argc, char* argv[]) {
+
+  if(argc < 1)
+    cout << "No args. Exit" << endl;
+
+  if(!strcmp(argv[1], "listen"))
+  {
+    cout << "The server is listening" << endl;
+  }
+
+  return 0;
+}
