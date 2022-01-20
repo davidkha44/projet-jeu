0d5cb6c3e32b2fa16346326afc8531065263737b
FredAbenah <frederick.abenah@gmail.com>
2022-01-20 19:25:01 +0100

    srv1

--- src/server/server/InstanceHandler.cpp
+++ src/server/server/InstanceHandler.cpp
@@ -1,5 +1,7 @@
 #include "InstanceHandler.h"
 #include <sio_client.h>
+#include <fstream>
+#include <iostream>
 #include "../shared/engine.h"
 #include "../shared/state.h"
 
@@ -13,7 +15,7 @@ void server::InstanceHandler::CreateRoom(std::string room_name)
 void server::InstanceHandler::NetCmd(std::string cmd)
 {
     sio::client* io = (sio::client*)engine::NetMessageHandler::IO;
-    state::WorldHandler::NetCommand(cmd);
+    //state::WorldHandler::NetCommand(cmd);
     io->socket()->emit("ack_net_cmd",Name+"::"+ cmd);
 }
 void server::InstanceHandler::CreateUser()
@@ -23,6 +25,46 @@ void server::InstanceHandler::CreateUser()
 }
 void server::InstanceHandler::Start()
 {
+    // state::World* w = state::WorldHandler::CurrentWorld;
+    // std::ifstream file(w->ResPath());
+    // std::cout << "W DIM : " << w->CellN().x <<"::"<< w->CellN().y << std::endl;
+    // state::Manager* mgr = state::Manager::GetMgrByID(0);
+    // int lines = 0,col = 0;
+    // for( std::string line; getline( file, line ); )
+    // {
+    //     if(line.find('#') == std::string::npos)
+    //     {
+    //         std::vector<std::string> strs = engine::FileHandler::SplitString(line,",");
+    //         for(int i = 0; i < w->CellN().x;i++)
+    //         {
+    //             state::Manageable* ref = mgr->GetByID(std::stoi(strs[i]));
+    //             state::Manageable* item = new state::Manageable("TILE_"+std::to_string(col)+"_"+std::to_string(lines),ref->Name());
+    //             item->ID(0xFF000000 + ((std::stoi(strs[i]) & 0xFF) << 16) +((col & 0xFF) << 8) + (lines & 0xFF));
+    //             item->AssignPosition(col,lines);
+    //             state::Manager::GetMgrByID(1)->Add(item);
+    //             col++;
+    //         } 
+    //     }
+    //     col = 0;
+    //     lines++;
+    //     if(lines == w->CellSize().y)
+    //         return;
+    // }
+    // std::cout << "INIT ACTOR" << std::endl;
+    // engine::FileHandler::DeserializeTable<engine::Pattern>("res/tables/Patterns.csv","CSV");
+    // engine::FileHandler::DeserializeTable<engine::NetCommand>("res/tables/NetMessage.csv","CSV");
+    // engine::FileHandler::DeserializeTable<engine::Action>("res/tables/Actions.csv","CSV");
+    // if(state::WorldHandler::CurrentWorld->Behaviour())
+    //     state::WorldHandler::CurrentWorld->Behaviour()->Run();
+    // if(state::WorldHandler::Behaviour)
+    // {
+    //     state::WorldHandler::Behaviour->EVENTS["OnTurnBegin"] = state::WorldHandler::OnTurnBegin;
+    //     state::WorldHandler::Behaviour->EVENTS["OnTurnEnd"] = state::WorldHandler::OnTurnEnd;
+    //     state::WorldHandler::Behaviour->Run();
+    //     for(state::Player* p : state::WorldHandler::Players)
+    //         if(p->Behaviour())
+    //             p->Behaviour()->RunFunction("InitializePlayer",(int*)NULL);
+    // }
     sio::client* io = (sio::client*)engine::NetMessageHandler::IO;
     io->socket()->emit("req_start_game", Name);
 }
